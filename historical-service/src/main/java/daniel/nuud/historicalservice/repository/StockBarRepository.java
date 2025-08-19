48d57e282ce50a5aa01503b61731428f1b04dac7
Daniel Nüüd <daaniel.nuud@gmail.com>
2025-08-19 13:09:19 +0300

    add notification service into historical service

--- historical-service/src/main/java/daniel/nuud/historicalservice/repository/StockBarRepository.java
+++ historical-service/src/main/java/daniel/nuud/historicalservice/repository/StockBarRepository.java
@@ -4,7 +4,11 @@ import daniel.nuud.historicalservice.model.StockBar;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Flux;
 
+import java.time.LocalDate;
+
 public interface StockBarRepository extends ReactiveMongoRepository<StockBar, String> {
     Flux<StockBar> findAllByTimestampBetween(Long start, Long end);
     Flux<StockBar> findByTickerAndDateBetween(String ticker, String fromDate, String toDate);
+    Flux<StockBar> findByTickerAndDateBetweenOrderByDateAsc(
+            String ticker, LocalDate from, LocalDate to);
 }
