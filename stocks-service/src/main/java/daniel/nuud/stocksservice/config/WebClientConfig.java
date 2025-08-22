ef2fe985a216c860941182e825f7b67d005cfa31
Daniel Nüüd <daaniel.nuud@gmail.com>
2025-08-22 13:04:02 +0300

    update

---
+++ stocks-service/src/main/java/daniel/nuud/stocksservice/config/WebClientConfig.java
@@ -0,0 +1,21 @@
+package daniel.nuud.stocksservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient polygonWebClient(@Value("${POLYGON_API_KEY}") String apiKey) {
+        return WebClient.builder()
+                .baseUrl("https://api.polygon.io")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + apiKey)
+                .build();
+    }
+}
