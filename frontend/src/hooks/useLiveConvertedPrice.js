be0606f58dd71ee518830470054bc1b6da469007
Daniel Nüüd <daaniel.nuud@gmail.com>
2025-09-01 20:03:16 +0300

    fix

--- frontend/src/hooks/useLiveConvertedPrice.js
+++ frontend/src/hooks/useLiveConvertedPrice.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { Client } from "@stomp/stompjs";
 
 const useLiveConvertedPrice = (ticker, currency) => {
   const [price, setPrice] = useState(null);
