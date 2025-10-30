dcf7c33f393cf096bfbe63760adfe678fe0805b9
Daniel Nüüd <daaniel.nuud@gmail.com>
2025-10-30 09:58:48 +0200

    update

--- news-service/src/main/java/daniel/nuud/newsservice/service/PolygonClient.java
+++ news-service/src/main/java/daniel/nuud/newsservice/service/PolygonClient.java
@@ -24,19 +24,13 @@ public class PolygonClient {
 
     @CircuitBreaker(name = "polygonNewsDB", fallbackMethod = "skipRefreshReactive")
     public Mono<ApiResponse> getApiResponse(String ticker) {
-        String key = getString(ticker);
         return webClient.get()
                 .uri("/v2/reference/news?ticker={ticker}&order=asc&limit=10&sort=published_utc&apiKey={apiKey}",
-                        key, apiKey)
+                        ticker, apiKey)
                 .retrieve()
                 .bodyToMono(ApiResponse.class)
                 .filter(resp -> resp != null && resp.getResults() != null)
                 .switchIfEmpty(Mono.error(new ResourceNotFoundException("News with ticker " + ticker + " not found")));
-    }
-
-    private static String getString(String query) {
-        String q = query == null ? "" : query.trim().toUpperCase(Locale.ROOT);
-        return q;
     }
 
     @SuppressWarnings("unused")
